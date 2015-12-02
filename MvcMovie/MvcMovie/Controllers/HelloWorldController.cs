ed1382fdb32de4da76edc9a6dfba0d4b365fdd0e
unknown <Admin@Payalt.promact.com>
2015-12-02 12:54:11 +0530

    add mvcapp

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+       public ActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int id = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + id);
+        }
+    }
+}
