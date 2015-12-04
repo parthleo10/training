9213a0055d3d0e8b305433c0af71bc025dad3aea
unknown <Admin@Payalt.promact.com>
2015-12-04 19:01:44 +0530

    EntityFramework with Dbinitializer

---
+++ MvcUniversity/MvcUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using MvcUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MvcUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext()
+            : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
