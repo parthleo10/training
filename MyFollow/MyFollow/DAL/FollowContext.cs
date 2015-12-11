17ddff87229b76e9ba2f96589a515761f3669901
unknown <Admin@Payalt.promact.com>
2015-12-11 19:20:16 +0530

    initial

---
+++ MyFollow/MyFollow/DAL/FollowContext.cs
@@ -0,0 +1,23 @@
+﻿using MyFollow.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MyFollow.DAL
+{
+    public class FollowContext : DbContext
+    {
+    
+    public FollowContext() : base("FollowContext")
+        {
+        }
+
+    public DbSet<EndUser> EndUsers { get; set; }
+    public DbSet<AppOwner> AppOwners { get; set; }
+       
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
